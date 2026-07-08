ffb7d856cbdcea3c69d86fbc46411b384884fb96
Pedro Kalyd <pedrokalyd13@gmail.com>
2026-07-08 02:12:38 -0300

    Adiciona portfolio experimental 3D

---
+++ experimental-src/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/experimental/',
+  build: {
+    outDir: '../experimental',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          motion: ['gsap', 'lenis'],
+          three: ['three']
+        }
+      }
+    }
+  }
+});
